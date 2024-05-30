6f1d0078ec6f9cf93eda34700080cecf4b536e31
VISHWANATH JUMANAL <vishwanathjumanal412@gmail.com>
2024-05-30 19:40:54 +0530

    code added

---
+++ main.py
@@ -0,0 +1,6 @@
+import util
+import shopping.shopping_cart
+
+print(shopping.shopping_cart.buy('apple'))
+print(util.multiply(2,3))
+print(util.divid(4,2))
---
+++ util.py
@@ -0,0 +1,5 @@
+def multiply(n1,n2):
+    return n1*n2
+
+def divid(n1,n2):
+    return n1/n2
